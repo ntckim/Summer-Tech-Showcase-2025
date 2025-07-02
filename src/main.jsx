ac439a766be32f94070b45deec019434eb086d2d
Ali Khowaja <alishadabkhowaja123@gmail.com>
2025-07-02 15:53:04 -0700

    initial set up

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import Home from "./pages/Home.jsx";
+import MockInterview from "./pages/MockInterview.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="mockinterview" element={<MockInterview />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
---
+++ src/pages/MockInterview.jsx
@@ -0,0 +1,3 @@
+export default function MockInterview() {
+  return <h1>Mock Interview</h1>;
+}
